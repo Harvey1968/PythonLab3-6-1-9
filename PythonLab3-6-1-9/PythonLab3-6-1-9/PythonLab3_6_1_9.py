e2393e46ddb1605d61e9990f856a5f98f535e594
Ian Harvey <108738675+Harvey1968@users.noreply.github.com>
2022-08-02 10:08:23 +0100

    Update PythonLab3_6_1_9.py

--- PythonLab3-6-1-9/PythonLab3-6-1-9/PythonLab3_6_1_9.py
+++ PythonLab3-6-1-9/PythonLab3-6-1-9/PythonLab3_6_1_9.py
@@ -3,8 +3,8 @@
 my_list = [1, 2, 4, 4, 9, 1, 4, 2, 6, 2, 9]	# I have added an extra '9' at position 4 [0,1,2,etc..] to prove 'sort()'.
 my_list.sort()	# Sorts list for readability only ;; this can be done at the beginning or end.
 new_list = []	# 'new_list' created as a temporary work area, so that I don't need to update the list in situ.
-for number in my_list:  # Checks all numbers in the original list.
-	if number not in new_list:  # If the number isn't in the new list then
-		new_list.append(number)  # it will append (add/copy) it here.
-my_list = new_list[:]  # Copies 'new_list' to the oridginal 'my_list'.
+for int in my_list:  # Checks all integer numbers in the original list.
+	if int not in new_list:  # If the number isn't in the new list then
+		new_list.append(int)  # it will append (add/copy) it here.
+my_list = new_list[:]  # Copies 'new_list' data to the oridginal 'my_list'.
 print("The list with unique elements only:\n", my_list, "\n")
